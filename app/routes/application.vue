0ec082e96477e243d111d77e1f27572ba0e74954
Zach Edwards <tolklorist@gmail.com>
2017-03-10 10:28:40 -0600

    vue templates

---
+++ app/routes/application.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="">
+    <div class="app">
+
+      <div class="nav">
+        <div class="nav-item">
+        </div>
+      </div>
+
+
+
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
